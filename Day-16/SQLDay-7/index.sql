da06bdaf9267de9b93f7d045f8696129b5175991
Deep168 <ashapatel592001@gmail.com>
2023-01-11 17:37:24 +0530

    good

--- Day-16/SQLDay-7/index.sql
+++ Day-16/SQLDay-7/index.sql
@@ -65,6 +65,10 @@ CREATE TABLE geek_demo (Name VARCHAR(10) );
 INSERT INTO geek_demo (Name)
 VALUES('A'), ('B'), ('B'), ('C'), ('C'), ('D'), ('E');
 
+
+
+
+
 SELECT Name,
 RANK () OVER (
 ORDER BY Name
@@ -80,3 +84,39 @@ SELECT GETDATE();
 SELECT TIME();
 
 SELECT TIME("2017-08-15 19:30:10.000001");
+
+Create Procedure sumThree
+@n1 int,
+@n2 int,
+@result int output as
+Set @result = @n1+@n2
+Declare @result int
+Execute sumThree 5,30,@result output
+Select @result
+
+CREATE PROCEDURE Fibonacci4 @Num int
+AS
+BEGIN
+	DECLARE 
+		@Num1 int,
+		@Num2 int,
+		@Num3 int,
+		@Counter int
+	SET @Num1= 0
+	SET @Num2= 1
+	SET @Counter= 0
+	SET @Num3=0
+	PRINT('Fibonacci Series')
+	PRINT(@Num1)
+	PRINT(@Num2)
+	WHILE @Counter < @Num -2
+	BEGIN
+		SET @Num3= @Num1 + @Num2
+		PRINT(@Num3)
+		SET @Counter = @Counter + 1
+		SET @Num1 = @Num2
+		SET @Num2 = @Num3
+	END
+END
+
+EXEC Fibonacci4 4
