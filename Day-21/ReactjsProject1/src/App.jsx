648fb2e74fa678562b5ae8ba9a05fb8c6e543515
Deep168 <ashapatel592001@gmail.com>
2023-01-17 18:04:13 +0530

    good

---
+++ Day-21/ReactjsProject1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import Dashboard from './page/Dashboard'
+
+
+function App() {
+ 
+
+  return (
+    <div>
+     <Dashboard />
+    </div>
+  )
+}
+
+export default App
