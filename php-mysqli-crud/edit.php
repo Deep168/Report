9f221f0acd6a892c0788a18f39659e6cc40a4e52
Deep168 <ashapatel592001@gmail.com>
2023-03-09 18:41:13 +0530

    commit

---
+++ php-mysqli-crud/edit.php
@@ -0,0 +1,91 @@
+<?php 
+//Database Connection
+include('dbconnection.php');
+if(isset($_POST['submit']))
+  {
+  	$eid=$_GET['editid'];
+  	//Getting Post Values
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $contno=$_POST['contactno'];
+    $email=$_POST['email'];
+	$gender=$_POST['gender'];
+	$hobbies=$_POST['hobbies'];
+	$new=implode(",",$hobbies);
+    $add=$_POST['address'];
+
+    //Query for data updation
+     $query=mysqli_query($con, "update  tblusers set FirstName='$fname',LastName='$lname', MobileNumber='$contno', Email='$email',Gender='$gender',hobbies='$new', Address='$add' where ID='$eid'");
+     
+    if ($query) {
+    echo "<script>alert('You have successfully update the data');</script>";
+    echo "<script type='text/javascript'> document.location ='index.php'; </script>";
+  }
+  else
+    {
+      echo "<script>alert('Something Went Wrong. Please try again');</script>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+<title>PHP Crud Operation!!</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="signup-form">
+    <form  method="POST">
+ <?php
+$eid=$_GET['editid'];
+$ret=mysqli_query($con,"select * from tblusers where ID='$eid'");
+ $row=mysqli_fetch_array($ret); 
+ $hobbies=explode(',',$row['hobbies']);
+?>
+		<h2>Update </h2>
+		<p class="hint-text">Update your info.</p>
+        <div class="form-group">
+			<div class="row">
+				<div class="col"><input type="text" class="form-control" name="fname" value="<?php  echo $row['FirstName'];?>" required="true"></div>
+				<div class="col"><input type="text" class="form-control" name="lname" value="<?php  echo $row['LastName'];?>" required="true"></div>
+			</div>        	
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" name="contactno" value="<?php  echo $row['MobileNumber'];?>" required="true" maxlength="10" pattern="[0-9]+">
+        </div>
+        <div class="form-group">
+        	<input type="email" class="form-control" name="email" value="<?php  echo $row['Email'];?>" required="true">
+        </div>
+
+		<div>
+		<div class="form-group">
+		Male<input value="male"  type="radio" name="gender"  <?php if($row['Gender']=="male"){echo "checked";}?>/>
+			Female<input value="female" type="radio" name="gender" <?php if($row['Gender']=="female"){echo "checked";}?> />
+			</td>
+		</div>
+
+		<div>Programming<input  type="checkbox" name="hobbies[]" <?= in_array('programming',$hobbies)?"checked" : " ";  ?> value="programming"/>
+			Coding<input type="checkbox" name="hobbies[]" value="coding" <?= in_array('coding',$hobbies)?"checked" : " ";  ?>/>
+			Editing<input type="checkbox" name="hobbies[]" value="editing" <?= in_array('editing',$hobbies)?"checked" : " ";  ?>/></div>
+
+		
+		<div class="form-group">
+            <textarea class="form-control" name="address" required="true"><?php  echo $row['Address'];?></textarea>
+        </div>        
+    
+		<div class="form-group">
+            <button type="submit" class="btn btn-success btn-lg btn-block" name="submit">Update</button>
+        </div>
+    </form>
+
+</div>
+</body>
+</html>
---
+++ php-mysqli-crud/index.php
@@ -0,0 +1,97 @@
+<?php
+//database conection  file
+include('dbconnection.php');
+//Code for deletion
+if(isset($_GET['delid']))
+{
+$rid=intval($_GET['delid']);
+$sql=mysqli_query($con,"delete from tblusers where ID=$rid");
+echo "<script>alert('Data deleted');</script>"; 
+echo "<script>window.location.href = 'index.php'</script>";     
+} 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Bootstrap Elegant Table Design</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="container-xl">
+    <div class="table-responsive">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <h2>User <b>Management</b></h2>
+                    </div>
+                       <div class="col-sm-7" align="right">
+                        <a href="insert.php" class="btn btn-secondary"><i class="material-icons"></i> <span>Add New User</span></a>
+                                        
+                    </div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>                       
+                        <th>Email</th>
+                        <th>Gender</th>
+                        <th>Hobbies</th>
+                        <th>Image</th>
+                        <th>Mobile Number</th>
+                        <th>Created Date</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                     <?php
+$ret=mysqli_query($con,"select * from tblusers");
+$cnt=1;
+$row=mysqli_num_rows($ret);
+if($row>0){
+while ($row=mysqli_fetch_array($ret)) {
+
+?>
+<!--Fetch the Records -->
+                    <tr>
+                        <td><?php echo $cnt;?></td>
+                        <td><?php  echo $row['FirstName'];?> <?php  echo $row['LastName'];?></td>
+                        <td><?php  echo $row['Email'];?></td>  
+                        <td><?php  echo $row['Gender'];?></td>
+                        <td><?php  echo $row['hobbies'];?></td>
+                        <td><?php  echo $row['image'];?></td>                       
+                        <td><?php  echo $row['MobileNumber'];?></td>
+                        <td> <?php  echo $row['CreationDate'];?></td>
+                        <td>
+  <a href="read.php?viewid=<?php echo htmlentities ($row['ID']);?>" class="view" title="View" data-toggle="tooltip"><i class="material-icons">View</i></a>
+                            <a href="edit.php?editid=<?php echo htmlentities ($row['ID']);?>" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">Edit</i></a>
+                            <a href="index.php?delid=<?php echo ($row['ID']);?>" class="delete" title="Delete" data-toggle="tooltip" onclick="return confirm('Do you really want to Delete ?');"><i class="material-icons">Delete</i></a>
+                        </td>
+                    </tr>
+                    <?php 
+$cnt=$cnt+1;
+} } else {?>
+<tr>
+    <th style="text-align:center; color:red;" colspan="6">No Record Found</th>
+</tr>
+<?php } ?>                 
+                
+                </tbody>
+            </table>
+       
+        </div>
+    </div>
+</div>     
+</body>
+</html>
